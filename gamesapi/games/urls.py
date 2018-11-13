611ae73072f597ff7873298bc234c40a14bf37e7
SimonaScrob <sscrob@pentalog.com>
2018-11-13 17:08:04 +0200

    gamesapi

---
+++ gamesapi/games/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from games import views
+
+urlpatterns = [
+    # url(r'^games/$', views.game_list),
+    # url(r'^games/(?P<pk>[0-9]+)/$', views.game_detail),
+    url(r'^game-categories/$', views.GameCategoryViewSet.as_view(), name=views.GameCategoryViewSet.name),
+    url(r'^game-categories/(?P<pk>[0-9]+)/$', views.GameCategoryDetailViewSet.as_view(), name=views.GameCategoryDetailViewSet.name),
+    url(r'^games/$', views.GameViewSet.as_view(), name=views.GameViewSet.name),
+    url(r'^games/(?P<pk>[0-9]+)/$', views.GameDetailViewSet.as_view(), name=views.GameDetailViewSet.name),
+    url(r'^players/$', views.PlayerViewSet.as_view(), name=views.PlayerViewSet.name),
+    url(r'^players/(?P<pk>[0-9]+)/$', views.PlayerDetailViewSet.as_view(), name=views.PlayerDetailViewSet.name),
+    url(r'^player-score/$', views.PlayerScoreViewSet.as_view(), name=views.PlayerScoreViewSet.name),
+    url(r'^player-score/(?P<pk>[0-9]+)/$', views.PlayerScoreDetailViewSet.as_view(), name=views.PlayerScoreDetailViewSet.name),
+    url(r'^users/$', views.UserViewSet.as_view(), name=views.UserViewSet.name),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetailViewSet.as_view(), name=views.UserDetailViewSet.name),
+    url(r'^$', views.ApiRoot.as_view(), name=views.ApiRoot.name)
+]
---
+++ gamesapi/games/views.py
@@ -0,0 +1,187 @@
+# from django.shortcuts import render
+#
+# from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
+# # from rest_framework.renderers import JSONRenderer
+# from rest_framework.decorators import api_view
+# from rest_framework.parsers import JSONParser
+# from rest_framework import status
+# from rest_framework.response import Response
+#
+# from games.models import Game
+# from games.serializers import GameSerializer
+#
+# # class JSONResponse(HttpResponse):
+# #
+# #     def __init__(self, data, **kwargs):
+# #         content = JSONRenderer().render(data)
+# #         kwargs['content_type'] = 'application/json'
+# #         super(JSONResponse, self).__init__(content, **kwargs)
+#
+#
+# # @csrf_exempt
+# @api_view(['GET', 'POST'])
+# def game_list(request):
+#     if request.method == 'GET':
+#         games = Game.objects.all()
+#         games_serializer = GameSerializer(games, many=True)
+#         return Response(games_serializer.data)
+#         # return JSONResponse(games_serializer.data)
+#     elif request.method == 'POST':
+#         # game_data = JSONParser().parse(request)
+#         game_serializer = GameSerializer(data=request.data)
+#         # game_serializer = GameSerializer(data=game_data)
+#         if game_serializer.is_valid():
+#             game_serializer.save()
+#             return Response(game_serializer.data, status=status.HTTP_201_CREATED)
+#             # return JSONResponse(game_serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         # return JSONResponse(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# # @csrf_exempt
+# @api_view(['GET', 'PUT', 'POST'])
+# def game_detail(request, pk):
+#     try:
+#         game = Game.objects.get(pk=pk)
+#     except Game.DoesNotExist:
+#         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+#
+#     if request.method == 'GET':
+#         game_serializer = GameSerializer(game)
+#         return Response(game_serializer.data)
+#         # return JSONResponse(game_serializer.data)
+#     elif request.method == 'PUT':
+#         # game_data = JSONParser().parse(request)
+#         # game_serializer = GameSerializer(game, data=game_data)
+#         game_serializer = GameSerializer(game, data=request.data)
+#         if game_serializer.is_valid():
+#             game_serializer.save()
+#             return Response(game_serializer.data)
+#             # return JSONResponse(game_serializer.data)
+#         return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         # return JSONResponse(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'DELETE':
+#         game.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+import django_filters
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.throttling import ScopedRateThrottle
+
+from games.models import Player, PlayerScore, GameCategory, Game
+from games.permissions import IsOwnerOrReadOnly
+from games.serializers import PlayerSerializer, PlayerScoreSerializer, GameCategorySerializer, GameSerializer, \
+    UserSerializer
+from rest_framework import filters
+from django_filters import NumberFilter, DateTimeFilter, AllValuesFilter, FilterSet
+
+
+class UserViewSet(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    name = 'user-list'
+
+
+class UserDetailViewSet(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    name = 'user-detail'
+
+
+class GameCategoryViewSet(generics.ListCreateAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-list'
+    throttle_scope = 'game-categories'
+    throttle_classes = (ScopedRateThrottle,)
+    filter_fields = ('name',)
+    search_fields = ('^name',)
+    ordering_fields = ('name',)
+
+
+class GameCategoryDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-detail'
+    throttle_scope = 'game-categories'
+    throttle_classes = (ScopedRateThrottle,)
+
+
+class GameViewSet(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-list'
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+    filter_fields = ('name', 'game_category', 'release_date', 'played', 'owner')
+    search_fields = ('^name',)
+    ordering_fields = ('name', 'release_date',)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class GameDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-detail'
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+
+class PlayerViewSet(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    name = 'player-list'
+    filter_fields = ('name', 'gender')
+    search_fields = ('^name',)
+    ordering_fields = ('name',)
+
+
+class PlayerDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    name = 'player-detail'
+
+
+class PlayerScoreFilter(django_filters.FilterSet):
+    min_score = django_filters.NumberFilter(field_name='score', lookup_expr='gte')
+    max_score = django_filters.NumberFilter(field_name='score', lookup_expr='lte')
+    from_score_date = django_filters.DateTimeFilter(field_name='score_date', lookup_expr='gte')
+    to_score_date = django_filters.DateTimeFilter(field_name='score_date', lookup_expr='lte')
+    player_name = django_filters.AllValuesFilter(field_name='player__name')
+    game_name = django_filters.AllValuesFilter(field_name='game__name')
+
+    class Meta:
+        model = PlayerScore
+        fields = ['score', 'min_score', 'max_score', 'from_score_date', 'to_score_date', 'player_name', 'game_name']
+
+
+class PlayerScoreViewSet(generics.ListCreateAPIView):
+    queryset = PlayerScore.objects.all()
+    serializer_class = PlayerScoreSerializer
+    name = 'playerscore-list'
+    filter_class = PlayerScoreFilter
+    ordering_fields = ('score', 'score_date', )
+
+
+class PlayerScoreDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PlayerScore.objects.all()
+    serializer_class = PlayerScoreSerializer
+    name = 'playerscore-detail'
+
+
+class ApiRoot(generics.GenericAPIView):
+    name = 'api-root'
+
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'players': reverse(PlayerViewSet.name, request=request),
+            'game-categories': reverse(GameCategoryViewSet.name, request=request),
+            'games': reverse(GameViewSet.name, request=request),
+            'scores': reverse(PlayerScoreViewSet.name, request=request),
+            'users': reverse(UserViewSet.name, request=request)
+        })
